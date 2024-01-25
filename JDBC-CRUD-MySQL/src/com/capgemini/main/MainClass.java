57222c29d03c22771adc2dc11f8cdbc2beedad75
DEEPANSHU KUMAR <deepanshu2002kumar@gmail.com>
2024-01-25 17:22:37 +0530

    Connection of Eclipse with MySQL

--- JDBC-CRUD-MySQL/src/com/capgemini/main/MainClass.java
+++ JDBC-CRUD-MySQL/src/com/capgemini/main/MainClass.java
@@ -1,11 +1,26 @@
 package com.capgemini.main;
 
-public class MainClass {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-	public static void main(String[] args) {
+public class MainClass {  
+//	here 'class' is a user defined datatype
+
+	public static void main(String[] args)throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
+		Connection con = null; 
+		
+		//Here Connection is an iterface and con is an obj but not of this 
+//		interface as it doesnot have an obj, as it has abstract methods in it
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		// Here the Class is the interface
+		
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims", "", "");
+		System.out.println("Connection Established");
+		
+		System.out.println(con.getClass().getName());
+		//Thus con is an object of jdbc class -> ConnectionImpl method's
 	}
-
 }
